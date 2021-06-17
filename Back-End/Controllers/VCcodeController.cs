d8b7d893ebf486b09dac8967f409a7f93ae67430
muyi <524453007@qq.com>
2021-06-17 12:22:42 +0800

    更新验证码，现在可以真的发送验证码了

--- Back-End/Controllers/VCcodeController.cs
+++ Back-End/Controllers/VCcodeController.cs
@@ -8,6 +8,14 @@ using Back_End.Contexts;
 using System.Text.Json;
 using Back_End.Models;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Web;
+
+
 
 
 namespace Back_End
@@ -16,10 +24,26 @@ namespace Back_End
     [Route("[controller]")]
     public class VCcodeController : ControllerBase
     {
+        private static readonly string url = "http://www.etuocloud.com/gatetest.action";
+        private static readonly string appKey = "vxdunoQ8hwJPBQoUcqz2rfEzpN9D7agz";
+        private static readonly string appSecret = "ClPdmYAiPeky7acZdlwvbUljG6uOIaJ13JiQOqQveCAtC89Ybr3o73UPV71tNdj1";
+        private static readonly string format = "json";
+
         [HttpPost]
         public bool SendCode()
         {
-            Console.WriteLine(InitialCode(4));
+            string code = InitialCode(4);
+
+            NameValueCollection parameters = new NameValueCollection();
+            parameters.Add("app_key", appKey);
+            parameters.Add("view", format);
+            parameters.Add("method", "cn.etuo.cloud.api.sms.simple");
+            parameters.Add("to", Request.Form["phone"]);
+            parameters.Add("template", "1");
+            parameters.Add("smscode", code);
+            parameters.Add("sign", getsign(parameters));
+            Console.WriteLine(code);
+            HttpClient.HttpPost(url, parameters);
             return true;
         }
 
@@ -36,138 +60,154 @@ namespace Back_End
             CookieOptions cookieOptions = new CookieOptions();
             cookieOptions.Path = "/";
             cookieOptions.HttpOnly = true;
-            cookieOptions.MaxAge = new TimeSpan(0, 10, 0);
+            cookieOptions.MaxAge = new TimeSpan(0, 5, 0);
             Response.Cookies.Append("VCcode", VCcode, cookieOptions);
             return VCcode;
         }
-    }
-}
-/*
-namespace Back_End
-{
-    public class VCcode
-    {
-        private string userId;
-        private string userKey;
-        private void getCode(HttpContext context)
-        {
-            //从http://sms.webchinese.cn/申请账号，获得密钥
-            string vc = "";//生成4位随机数码做为验证码
-            Random r = new Random();
-            int num1 = r.Next(0, 9);
-            int num2 = r.Next(0, 9);
-            int num3 = r.Next(0, 9);
-            int num4 = r.Next(0, 9);
 
-            int[] numbers = new int[4] { num1, num2, num3, num4 };
-            for (int i = 0; i < numbers.Length; i++)
+        //实现自网站api示例
+        private static string getsign(NameValueCollection parameters)
+        {
+            SortedDictionary<string, string> sParams = new SortedDictionary<string, string>();
+            foreach (string key in parameters.Keys)
             {
-                vc += numbers[i].ToString();
+                sParams.Add(key, parameters[key]);
             }
 
-            string number = "15805910204";//接受短信的手机号
-            string smsText = "您的验证码为" + vc + "。";//【重走霞客路】
-
-            string postUrl = GetPostUrl(number, smsText);
-            string result = PostSmsInfo(postUrl);
-            string t = GetResult(result);
-
-            HttpCookie mycookie = new HttpCookie("getCode");
-            mycookie.Values.Add("VCode", vc);//把验证码放到COOKIE里
-            TimeSpan ts = new TimeSpan(3, 0, 30, 0);
-            DateTime dt = DateTime.Now;
-            mycookie.Expires = dt.Add(ts);
-            context.Response.AppendCookie(mycookie);
-
-        }
-        //示例http://localhost:2446/WebService.ashx?action=userLoginmobile=13888888888&VCode=name01
-
-        public string GetPostUrl(string smsMob, string smsText)
-        {
-
-            //uid为用户名,key为密钥
-            string postUrl = "http://utf8.api.smschinese.cn/?Uid=" + userId + "&key=" + userKey + "&smsMob=" + smsMob + "&smsText=" + smsText;
-            return postUrl;
-        }
-        public string PostSmsInfo(string url)
-        {
-            string strRet = null;
-            if (url == null || url.Trim().ToString() == "")
+            string sign = string.Empty;
+            StringBuilder codedString = new StringBuilder();
+            foreach (KeyValuePair<string, string> temp in sParams)
             {
-                return strRet;
-            }
-            string targeturl = url.Trim().ToString();
-            try
-            {
-                HttpWebRequest hr = (HttpWebRequest)WebRequest.Create(targeturl);
-                hr.UserAgent = "Mozilla/4.0(compatible;MISE 6.0;Window NT 5.1)";
-                hr.Method = "GET";
-                hr.Timeout = 30 * 60 * 1000;
-                WebResponse hs = hr.GetResponse();
-                Stream sr = hs.GetResponseStream();
-                StreamReader ser = new StreamReader(sr, Encoding.Default);
-                strRet = ser.ReadToEnd();
-            }
-            catch (Exception ex)
-            {
-                strRet = null;
-            }
-            return strRet;
-        }
-
-        public string GetResult(string strRet)
-        {
-            int result = 0;
-            try
-            {
-                result = int.Parse(strRet);
-                switch (result)
+                if (temp.Value == "" || temp.Value == null || temp.Key.ToLower() == "sign")
                 {
-                    case -1:
-                        strRet = "没有该用户账户";
-                        break;
-                    case -2:
-                        strRet = "接口密钥不正确，不是账户登陆密码";
-                        break;
-                    case -21:
-                        strRet = "MDS接口密钥加密不正确";
-                        break;
-                    case -3:
-                        strRet = "短信数量不足";
-                        break;
-                    case -11:
-                        strRet = "该用户被禁用";
-                        break;
-                    case -14:
-                        strRet = "短信内容出现非法字符";
-                        break;
-                    case -4:
-                        strRet = "手机格式不正确";
-                        break;
-                    case -41:
-                        strRet = "手机号码为空";
-                        break;
-                    case -42:
-                        strRet = "短信内容为空";
-                        break;
-                    case -51:
-                        strRet = "短信签名格式不正确，接口签名格式为：【签名内容】";
-                        break;
-                    case -6:
-                        strRet = "IP限制";
-                        break;
-                    default:
-                        strRet = "发送短信数量：" + result;
-                        break;
+                    continue;
                 }
+
+                if (codedString.Length > 0)
+                {
+                    codedString.Append("&");
+                }
+                codedString.Append(temp.Key.Trim());
+                codedString.Append("=");
+                codedString.Append(temp.Value.Trim());
             }
-            catch (Exception ex)
-            {
-                strRet = ex.Message;
-            }
-            //TextBox1.Text = strRet.ToString();
-            return strRet;
+
+            // 应用key
+            codedString.Append(appSecret);
+            string signkey = codedString.ToString();
+            sign = GetMD5(signkey, "utf-8");
+
+            return sign;
+
         }
+
+        //实现自网站api示例
+        private static string GetMD5(string encypStr, string charset)
+        {
+            string retStr;
+            MD5CryptoServiceProvider m5 = new MD5CryptoServiceProvider();
+
+            //创建md5对象
+            byte[] inputBye;
+            byte[] outputBye;
+
+            //使用XXX编码方式把字符串转化为字节数组．
+            try
+            {
+                inputBye = Encoding.GetEncoding(charset).GetBytes(encypStr);
+            }
+            catch (Exception)
+            {
+                inputBye = System.Text.Encoding.UTF8.GetBytes(encypStr);
+            }
+            outputBye = m5.ComputeHash(inputBye);
+
+            retStr = System.BitConverter.ToString(outputBye);
+            retStr = retStr.Replace("-", "").ToUpper();
+
+            //  return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(ConvertString, "MD5").ToLower(); ;
+
+            return retStr;
+        }
+
+    }
+
+    //实现自网站api示例
+    public class HttpClient
+    {
+        /// <summary>
+        /// POST请求与获取结果  
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static string HttpPost(string Url, NameValueCollection parameters)
+        {
+            return HttpPost(Url, toParaData(parameters));
+        }
+
+
+
+        //调用http接口,接口编码为utf-8
+        private static string toParaData(NameValueCollection parameters)
+        {
+
+            //设置参数，并进行URL编码
+            StringBuilder codedString = new StringBuilder();
+            foreach (string key in parameters.Keys)
+            {
+                // codedString.Append(HttpUtility.UrlEncode(key));
+                codedString.Append(key);
+                codedString.Append("=");
+                codedString.Append(HttpUtility.UrlEncode(parameters[key], System.Text.Encoding.UTF8));
+                codedString.Append("&");
+            }
+            string paraUrlCoded = codedString.Length == 0 ? string.Empty : codedString.ToString().Substring(0, codedString.Length - 1);
+
+
+            return paraUrlCoded;
+        }
+
+
+        /// <summary>  
+        /// POST请求与获取结果  
+        /// </summary>  
+        public static string HttpPost(string Url, string postDataStr)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded;charset=utf-8";
+
+            //request.ContentLength = postDataStr.Length;
+            //StreamWriter writer = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.UTF8);
+            // writer.Write(postDataStr);
+            // writer.Flush();
+
+
+            //将URL编码后的字符串转化为字节
+            byte[] payload = System.Text.Encoding.UTF8.GetBytes(postDataStr);
+            request.ContentLength = payload.Length;
+            Stream writer = request.GetRequestStream();
+            writer.Write(payload, 0, payload.Length);
+            writer.Close();
+
+            //获得响应流
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8"; //默认编码  
+            }
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+
+            string retString = reader.ReadToEnd();
+            return retString;
+        }
+
+
+
     }
 }
-*/
+
+
