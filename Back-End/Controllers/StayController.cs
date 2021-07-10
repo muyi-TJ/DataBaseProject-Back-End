c07994dfdf0dfc8dc309bab07a8441d7b17ab060
番茄炒鸡蛋 <342472121@qq.com>
2021-07-11 02:19:59 +0800

    查询stay，更改查询FavoriteStay

--- Back-End/Controllers/StayController.cs
+++ Back-End/Controllers/StayController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Back_End.Contexts;
 using System.Text.Json;
 using Back_End.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Back_End.Controllers
 {
@@ -32,6 +33,79 @@ namespace Back_End.Controllers
             catch
             {
                 return null;
+            }
+        }
+
+        public class StayInfo {
+            public int stayId { get; set; }
+            public string stayName { get; set; }
+            public string stayCharcateristic { get; set; }
+            public int stayPrice { get; set; }
+            public string stayPhoto { get; set; }
+        }
+
+        [HttpGet("getStayByPrice")]
+        public string GetStayByPrice() {
+            var context = ModelContext.Instance;
+            context.DetachAll();
+            var message = new GetStayMessage();
+            message.data.Add("stayList", new List<StayInfo>());
+            try {
+                var staySelectList = context.Rooms
+                    .GroupBy(r => r.StayId)
+                    .OrderBy(r => r.Min(x => x.Price))
+                    .Select(g => new StayInfo {
+                        stayId = g.Key,
+                        stayPrice = g.Min(x => x.Price)
+                    })
+                    .ToList();
+                if (staySelectList.Count > 6)
+                    staySelectList.RemoveRange(6, staySelectList.Count - 6);
+                foreach (var staySelect in staySelectList) {
+                    var stay = context.Stays.Single(b => b.StayId == staySelect.stayId);
+                    staySelect.stayCharcateristic = stay.Characteristic;
+                    staySelect.stayName = stay.StayName;
+                    staySelect.stayPhoto = context.RoomPhotos.Where(b => b.StayId == staySelect.stayId).FirstOrDefault().RPhoto;
+                }
+                message.data["stayList"] = staySelectList;
+                message.errorCode = 200;
+                return message.ReturnJson();
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.ToString());
+                return message.ReturnJson();
+            }
+        }
+
+        [HttpGet("getStayByHot")]
+        public string GetStayByHot() {
+            var context = ModelContext.Instance;
+            context.DetachAll();
+            var message = new GetStayMessage();
+            message.data.Add("stayList", new List<StayInfo>());
+            try {
+                var staySelectList = context.Generates
+                    .GroupBy(r => r.StayId)
+                    .OrderBy(r => r.Count())
+                    .Select(g => new StayInfo {
+                        stayId = g.Key
+                    })
+                    .ToList();
+                if (staySelectList.Count > 4)
+                    staySelectList.RemoveRange(4, staySelectList.Count - 4);
+                foreach (var staySelect in staySelectList) {
+                    var stay = context.Stays.Single(b => b.StayId == staySelect.stayId);
+                    staySelect.stayPhoto = context.RoomPhotos.Where(b => b.StayId == staySelect.stayId).FirstOrDefault().RPhoto;
+                    staySelect.stayPrice = context.Rooms.Where(b => b.StayId == staySelect.stayId).Min(x => x.Price);
+                    staySelect.stayCharcateristic = stay.Characteristic;
+                    staySelect.stayName = stay.StayName;
+                }
+                message.errorCode = 200;
+                return message.ReturnJson();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return message.ReturnJson();
             }
         }
     }
