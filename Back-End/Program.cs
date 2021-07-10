10ed96ad16c50118e467567dee7184526efc8c89
番茄炒鸡蛋 <342472121@qq.com>
2021-07-10 23:24:11 +0800

    ssl证书

--- Back-End/Program.cs
+++ Back-End/Program.cs
@@ -4,20 +4,26 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Back_End {
     public class Program {
+        
         public static void Main(string[] args) {
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
+                    var x509ca = new X509Certificate2(File.ReadAllBytes(@"api.guisu.fun.pfx"), "an13154e4");
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:6001/");
+                    //webBuilder.UseUrls("https://0.0.0.0:6001/");
+                    webBuilder.UseKestrel(option => option.ListenAnyIP(6001, config => config.UseHttps(x509ca)));
                 });
     }
 }
